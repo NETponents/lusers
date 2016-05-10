e3ad27dfef28173c4b57ab6e61a8b40d9326e686
ARMmaster17 <wordman05@gmail.com>
2016-05-09 21:06:46 -0400

    Added Character class.

---
+++ Lusers/Lusers/Character.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lusers
+{
+    namespace Players
+    {
+        public class Character
+        {
+            protected string _name;
+            protected Vector2 _location;
+            protected Texture2D _sprite;
+
+            public Character(Vector2 location, Texture2D sprite)
+            {
+                _location = location;
+                _sprite = sprite;
+                _name = "Unnamed";
+            }
+            public Character(Vector2 location, Texture2D sprite, string name)
+            {
+                _location = location;
+                _sprite = sprite;
+                _name = name;
+            }
+        }
+    }
+}
